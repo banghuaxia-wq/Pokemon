a0fc22cf60d67cfa06b56c8e97074d835794d2aa
banghuaxia-wq <banghuaxia@gmail.com>
2025-10-29 22:52:13 +0800

    提示框

--- Assets/Scripts/Tooltip System.cs
+++ Assets/Scripts/Tooltip System.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TooltipSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static TooltipSystem current;
+    public Tooltip tooltip;
+    public void Awake()
     {
-        
+        current = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void Show(string content, string header = "")
     {
-        
+        current.tooltip.SetText(content, header);
+        current.tooltip.gameObject.SetActive(true);
+    }
+    
+    public static void Hide()
+    {
+        current.tooltip.gameObject.SetActive(false);
     }
 }
